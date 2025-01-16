50be1f52dbd23eda1d376dc8de5bbe7a5c85d5fc
David Palmqist <dpalmquist@fullerton.edu>
2025-01-16 14:22:42 -0800

    update eTextbooks to use new data source and adjust column names in templates

--- eTextbooks.py
+++ eTextbooks.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template,request,escape
+from flask import Flask, render_template,request
 import pandas as pd
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -19,14 +20,18 @@ def log_request(req, res):
 #         #     results.append(line.split('|'))
 #         results=[line.split('|') for line in log_file.readlines()]
 #         return str(escape(results))
+
+# use pandas to read the log file
     
+
 
 @app.route('/')
 @app.route('/entry')
 def entry_page()->'html':
-    books_df = pd.read_excel('Ebook Match with Bookstore Spring 2022.xlsx',header=0,sheet_name="Ebook Library Match")
-    books = books_df[['Section Code','Instructor Name','Short Title','Permalink']].sort_values("Section Code").to_dict('records')
-    return render_template('row.html',the_title='Welcome to letter search on the web', the_books=books)
+    #books_df = pd.read_excel('Ebook Match with Bookstore Spring 2022.xlsx',header=0,sheet_name="Ebook Library Match")
+    books_df = pd.read_excel('/Users/dpalmquist/source/eTextBookList/etextbooks-list-f24.xlsx',header=0,sheet_name="books")
+    books = books_df[['SectionCode','Instructor','Title','URL']].sort_values("SectionCode").to_dict('records')
+    return render_template('row.html',the_title='No cost textbooks', the_books=books)
 
 
 if __name__ == "__main__":
